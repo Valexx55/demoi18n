1f4f3c00195542b2764a08e29505b8618ff4b111
vale <vale@debian.sin>
2020-06-10 16:38:59 +0200

    vdemo

---
+++ app/src/main/java/edu/val/ejempli18n/MainActivity.java
@@ -0,0 +1,18 @@
+package edu.val.ejempli18n;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        this.startActivity(new Intent(this, MediaProjectionDemo.class));
+
+    }
+}
